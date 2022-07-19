8e3b158d8b494354e6b11697abe1bd77ece39d6e
Louis Bouchonville <lbouchon@fu-r7-p4.s19.be>
2022-07-19 13:43:13 +0200

    correction

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -5,27 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbouchon <lbouchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/19 13:28:40 by lbouchon          #+#    #+#             */
-/*   Updated: 2022/07/19 13:31:59 by lbouchon         ###   ########.fr       */
+/*   Created: 2022/07/19 13:39:07 by lbouchon          #+#    #+#             */
+/*   Updated: 2022/07/19 13:42:45 by lbouchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int   ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-   unsigned char  *ptr_s1;
-   unsigned char  *ptr_s2;
-   size_t         i;
+	unsigned char	*ptr_s1;
+	unsigned char	*ptr_s2;
+	size_t			i;
 
-   ptr_s1 = (unsigned char *)s1;
-   ptr_s2 = (unsigned char *)s2;
-   i = 0;
-   while (i < n)
-   {
-      if (ptr_s1[i] != ptr_s2[i])
-         return (ptr_s1[i] - ptr_s2[i]);
-      i++;
-   }
-   return (0);
+	ptr_s1 = (unsigned char *)s1;
+	ptr_s2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr_s1[i] != ptr_s2[i])
+			return (ptr_s1[i] - ptr_s2[i]);
+		i++;
+	}
+	return (0);
 }
